1909172ecb585dcd665ffea81c6b7e1a61962f7e
student <student@example.com>
2023-03-07 15:36:37 -0500

    Add build script

---
+++ docker_build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+docker build --network=host -t libafl:base -f Dockerfile .
+docker build --network=host -t libafl:offline -f Dockerfile-offline .
+docker build --network=host -t libafl:offlne-vscode -f Dockerfile-vscode .
+
+echo "Saving image tarball..."
+docker save libafl | gzip > /tmp/libafl_offline.tgz
+
