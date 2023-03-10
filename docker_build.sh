f88472a6949bf95ab8bde2c5ffb31c2ed3863026
student <student@example.com>
2023-03-10 10:05:36 -0500

    Add externally vendored crates

--- docker_build.sh
+++ docker_build.sh
@@ -3,7 +3,7 @@ set -e
 
 docker build --network=host -t libafl:base -f Dockerfile .
 docker build --network=host -t libafl:offline -f Dockerfile-offline .
-docker build --network=host -t libafl:offlne-vscode -f Dockerfile-vscode .
+docker build --network=host -t libafl:offline-vscode -f Dockerfile-vscode .
 
 echo "Saving image tarball..."
 docker save libafl | gzip > /tmp/libafl_offline.tgz
